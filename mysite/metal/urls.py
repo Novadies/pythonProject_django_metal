6eb50b22d12ec75a83a9223ca9c339b12e49da87
RysukAO <novadies@mail.ru>
2023-07-20 15:08:16 +0300

    буду делать вьюху

--- mysite/metal/urls.py
+++ mysite/metal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from .views_for_csv import upload_csv
 
 urlpatterns = [
     path('', Start_page.as_view(), name='start_page_url'),
     path('steel/<str:slug>/', Post_index.as_view(), name='post_index_url'),
+    path('upload_csv/', upload_csv, name='upload_csv'),     # для загрузки файлов
 ]
--- mysite/metal/views.py
+++ mysite/metal/views.py
@@ -1,8 +1,19 @@
 from django.views.generic import View
 from .utils import ObjiectDetailMixin
-
+import csv
 from .models import *
-#from .qwe import *   # заполнение базы данных!!!!
+
+# Metal.objects.all().delete()
+# Metal_info.objects.all().delete()
+# Metal_class.objects.all().delete()
+
+START_DB=False
+if START_DB:
+    from .first_bd import zapis
+    with open('metal\sourse\metal.csv') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        zapis(reader)
+
 
 class Start_page(ObjiectDetailMixin, View):
     model = Metal
