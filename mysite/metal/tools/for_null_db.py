07cf510616c6403e1e2b08591cdfd81f261607f0
RysukAO <novadies@mail.ru>
2023-12-21 14:39:02 +0300

    Добавлены файлы для отслеживания

---
+++ mysite/metal/tools/for_null_db.py
@@ -0,0 +1,23 @@
+from mysite.settings import STATICFILES_DIRS
+
+DieMigrationsDie = False
+START_DB=False
+DEATH=False
+
+if DieMigrationsDie:
+    from django.db import connection #удаление всех миграций приложения
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM 'django_migrations' WHERE 'app'='metal'")
+
+if START_DB:
+        # удаление начальных данных
+    if DEATH:
+        from metal.models import *
+        Metal.objects.all().delete()
+        Metal_2.objects.all().delete()
+        Metal_info.objects.all().delete()
+        Metal_class.objects.all().delete()
+        MetalSearch.objects.all().delete()
+
+    from metal.tools.csv_to_bd import zapis
+    with open(f'{STATICFILES_DIRS}\metal.csv') as f: zapis(f) # может быть ошибка в пути
---
+++ mysite/mysite/custom_json_formatter.py
@@ -0,0 +1,19 @@
+from django.utils.datetime_safe import datetime
+from pythonjsonlogger import jsonlogger
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        """ стандартная функция описание форматтера """
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+
+        if not log_record.get('timestamp'):
+            log_record['timestamp'] = datetime.utcnow().strftime('%d-%m-%Y|%H:%M:%S|%fZ')
+
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
+        log_record['module'] = record.module
+        log_record['message'] = log_record.pop('message', None)
